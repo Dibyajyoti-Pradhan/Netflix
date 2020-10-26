71e3b781f39878bd9d36ef85a92a4115dfed41eb
Dibyajyoti-Pradhan <dibyojyotipradhan@gmail.com>
2020-10-26 09:13:39 +0530

    Added Accordion

---
+++ src/components/accordion/index.js
@@ -0,0 +1,52 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { createContext, useState, useContext } from "react";
+import {
+  Container,
+  Frame,
+  Title,
+  Item,
+  Inner,
+  Header,
+  Body,
+} from "./styles/accordion";
+const ToggleContext = createContext();
+const Accordion = ({ children, ...restProps }) => (
+  <Container {...restProps}>
+    <Inner>{children}</Inner>
+  </Container>
+);
+
+Accordion.Title = ({ children, ...restProps }) => (
+  <Title {...restProps}>{children}</Title>
+);
+Accordion.Item = ({ children, ...restProps }) => {
+  const [toggleShow, setToggleShow] = useState(false);
+  return (
+    <ToggleContext.Provider value={{ toggleShow, setToggleShow }}>
+      <Item {...restProps}>{children}</Item>
+    </ToggleContext.Provider>
+  );
+};
+
+Accordion.Header = ({ children, ...restProps }) => {
+  const { toggleShow, setToggleShow } = useContext(ToggleContext);
+  return (
+    <Header
+      onClick={() => setToggleShow((toggleShow) => !toggleShow)}
+      {...restProps}
+    >
+      {children}
+      {toggleShow ? (
+        <img src="/images/icons/close-slim.png" alt="Close" />
+      ) : (
+        <img src="/images/icons/add.png" alt="Open" />
+      )}
+    </Header>
+  );
+};
+
+Accordion.Body = ({ children, ...restProps }) => {
+  const { toggleShow } = useContext(ToggleContext);
+  return toggleShow ? <Body {...restProps}>{children}</Body> : null;
+};
+export default Accordion;
