9061cc42737663ae9db4fd97fc25cfd6337487b8
Dibyajyoti-Pradhan <dibyojyotipradhan@gmail.com>
2020-10-31 12:59:58 +0530

    Added Search

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,5 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useContext, useEffect, useState } from "react";
+import Fuse from "fuse.js";
 import { Card, Header, Loading, Player } from "../components";
 import { FooterContainer } from "./footer";
 import { SelectProfileContainer } from "./profiles";
@@ -25,6 +26,18 @@ export const BrowseContainer = ({ slides }) => {
   useEffect(() => {
     setSlideRows(slides[category]);
   }, [slides, category]);
+
+  useEffect(() => {
+    const fuse = new Fuse(slideRows, {
+      keys: ["data.description", "data.title", "data.genre"],
+    });
+    const results = fuse.search(searchTerm).map(({ item }) => item);
+    if (slideRows.length > 0 && searchTerm.length > 3 && results.length > 0) {
+      setSlideRows(results);
+    } else {
+      setSlideRows(slides[category]);
+    }
+  }, [searchTerm]);
   return profile.displayName ? (
     <>
       {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
